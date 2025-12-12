c5d90eca099cdd968fff2cffee6942710995adb2
Sarwan Yadav <sarwanyadav6174@gmail.com>
2025-12-13 03:53:17 +0530

    feat: update react-native-reanimated and add new dependencies for worklets

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      // ... any other plugins
+      'react-native-reanimated/plugin', // <--- THIS MUST BE LAST
+    ],
   };
 };
