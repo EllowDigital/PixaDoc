21662afa82ce1f7e61ec6d9d28dac043ad88f901
Sarwan Yadav <sarwanyadav6174@gmail.com>
2025-12-13 02:58:08 +0530

    Remove Android project files and configurations for PixaDoc app

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-/**
- * Minimal metro config compatible with Reanimated and managed workflow.
- */
-/* eslint-env node */
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
